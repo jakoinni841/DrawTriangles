e09a0a9a8c19c3440d341301daa51e7d04134ef7
jakoinni841 <jakoinni841@ed.amdsb.ca>
2016-12-09 13:07:48 -0500

    Final Commit

--- DrawTriangles/Form1.Designer.cs
+++ DrawTriangles/Form1.Designer.cs
@@ -29,44 +29,38 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 405);
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(536, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(153, 56);
+            this.button1.Size = new System.Drawing.Size(181, 72);
             this.button1.TabIndex = 0;
             this.button1.Text = "Draw Triangle";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(13, 379);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(152, 20);
-            this.textBox1.TabIndex = 1;
-            // 
             // Window
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.ClientSize = new System.Drawing.Size(729, 473);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Window";
             this.Text = "Triangles";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- DrawTriangles/Form1.cs
+++ DrawTriangles/Form1.cs
@@ -22,18 +22,20 @@ namespace DrawTriangles
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Graphics formGraphics = this.CreateGraphics();
+            Graphics g = this.CreateGraphics();
             Pen drawTri = new Pen(Color.Red, 3);
-            DrawTriangle(drawTri, 200, 190, 230, 220, 180, 220);
+            DrawTriangle(drawTri, 100, 150, 200, 200, 200, 400);
+            DrawTriangle(drawTri, 75, 12, 11, 103, 300, 6);
         }
 
         public void DrawTriangle(Pen drawTri, int x1, int y1, int x2, int y2, int x3, int y3)
         {
-            Graphics formGraphics = this.CreateGraphics();
-
-            formGraphics.DrawLine(drawTri, x1, y1, x2, y2);
-            formGraphics.DrawLine(drawTri, x2, y2, x3, y3);
-            formGraphics.DrawLine(drawTri, x3, y3, x1, y1);
+            #region Draw Triangle Method
+            Graphics g = this.CreateGraphics();
+            g.DrawLine(drawTri, x1, y1, x2, y2);
+            g.DrawLine(drawTri, x2, y2, x3, y3);
+            g.DrawLine(drawTri, x3, y3, x1, y1);
+            #endregion
         }
 
     }
